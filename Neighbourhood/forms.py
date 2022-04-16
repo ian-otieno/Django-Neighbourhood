231cc715eb5fddd761cbec5fe9bcfc4efd6be6b7
ian-otieno <ian.otieno@student.moringaschool.com>
2022-04-16 11:43:44 +0300

    add class update profile form and password change form to forms.py

---
+++ Neighbourhood/forms.py
@@ -0,0 +1,107 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import SetPasswordForm
+from .models import NeighbourHood, Profile
+
+COUNTIES = [
+    ('Choose County', ('Choose County')), 
+    ('Baringo', ('Baringo')),
+    ('Bomet', ('Bomet')),
+    ('Bungoma ', ('Bungoma ')),
+    ('Busia', ('Busia')),
+    ('Elgeyo Marakwet', ('Elgeyo Marakwet')),
+    ('Embu', ('Embu')),
+    ('Garissa', ('Garissa')),
+    ('Homa Bay', ('Homa Bay')),
+    ('Isiolo', ('Isiolo')),
+    ('Kajiado', ('Kajiado')),
+    ('Kakamega', ('Kakamega')),
+    ('Kericho', ('Kericho')),
+    ('Kiambu', ('Kiambu')),
+    ('Kilifi', ('Kilifi')),
+    ('Kirinyaga', ('Kirinyaga')),
+    ('Kisii', ('Kisii')),
+    ('Kisumu', ('Kisumu')),
+    ('Kitui', ('Kitui')),
+    ('Kwale', ('Kwale')),
+    ('Laikipia', ('Laikipia')),
+    ('Lamu', ('Lamu')),
+    ('Machakos', ('Machakos')),
+    ('Makueni', ('Makueni')),
+    ('Mandera', ('Mandera')),
+    ('Meru', ('Meru')),
+    ('Migori', ('Migori')),
+    ('Marsabit', ('Marsabit')),
+    ('Mombasa', ('Mombasa')),
+    ('Muranga', ('Muranga')),
+    ('Nairobi', ('Nairobi')),
+    ('Nakuru', ('Nakuru')),
+    ('Nandi', ('Nandi')),
+    ('Narok', ('Narok')),
+    ('Nyamira', ('Nyamira')),
+    ('Nyandarua', ('Nyandarua')),
+    ('Nyeri', ('Nyeri')),
+    ('Samburu', ('Samburu')),
+    ('Siaya', ('Siaya')),
+    ('Taita Taveta', ('Taita Taveta')),
+    ('Tana River', ('Tana River')),
+    ('Tharaka Nithi', ('Tharaka Nithi')),
+    ('Trans Nzoia', ('Trans Nzoia')),
+    ('Turkana', ('Turkana')),
+    ('Uasin Gishu', ('Uasin Gishu')),
+    ('Vihiga', ('Vihiga')),
+    ('Wajir', ('Wajir')),
+    ('West Pokot', ('West Pokot')),
+]
+
+CATEGORIES = [
+    ('1', 'Crimes and Safety'),
+    ('2', 'Health Emergency'),
+    ('3', 'Recommendations'),
+    ('4', 'Fire Breakouts'),
+    ('5', 'Lost and Found'),
+    ('6', 'Death'),
+    ('7', 'Event'),
+]
+
+class UpdateUserForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control mb-4'}))
+    last_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control mb-4'}))
+    username = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control mb-4'}))
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': 'form-control mb-4', 'readonly':'readonly'}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email']
+
+class UpdateProfileForm(forms.ModelForm):
+    profile_picture = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class': 'dropify', 'data-height':420, 'data-max-file-size':"1M"}))
+    bio = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'form-control mb-4', 'rows': 5, 'placeholder':'Keep it short, preferably in one concise sentence'}))
+    national_id = forms.CharField(max_length=50, required=False, widget=forms.TextInput(attrs={'class': 'form-control mb-4', 'placeholder':'National ID'}))
+    neighbourhood = forms.ChoiceField(label=u'Select Your Neighbourhood', required=True, widget=forms.Select(attrs={'class': 'form-control mb-4'}))
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateProfileForm, self).__init__(*args, **kwargs)
+        self.fields['neighbourhood'].choices = [(e.id, e.title) for e in NeighbourHood.objects.all()]
+    
+    class Meta:
+        model = Profile
+        fields = ['profile_picture', 'bio', 'national_id', 'neighbourhood']
+
+class PasswordChangeForm(SetPasswordForm):
+    old_password = forms.CharField(required=True,widget=forms.PasswordInput(attrs={'class': 'form-control mb-4','type': 'password','name': 'password1','placeholder': 'Old Password',}))
+    new_password1 = forms.CharField(required=True,widget=forms.PasswordInput(attrs={'class': 'form-control mb-4','type': 'password','name': 'password1','placeholder': 'New Password',}))
+    new_password2 = forms.CharField(required=True,widget=forms.PasswordInput(attrs={'class': 'form-control mb-4','type': 'password','name': 'password1','placeholder': 'Confirm Password',}))
+
+    class Meta:
+        model = User
+        fields = ('old_password','new_password1','new_password2',)
+
+def clean_old_password(self):
+    old_password = self.cleaned_data["old_password"]
+    if not self.user.check_password(old_password):
+        raise forms.ValidationError(
+            self.error_messages['password_incorrect'],
+            code='password_incorrect',
+        )
+    return old_password
