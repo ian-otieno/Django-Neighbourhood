3cb7d497022ed5ba739a951f011ba78960320c2c
ian-otieno <ian.otieno@student.moringaschool.com>
2022-04-16 14:41:34 +0300

    add class emailthread  to views.py

--- Neighbourhood/views.py
+++ Neighbourhood/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.utils.encoding import force_str, force_bytes
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.contrib.auth import update_session_auth_hash
+from Neighbourhood.models import Business, Membership, NeighbourHood, Post, Profile
+from .forms import AddBussinessForm, AddNeighbourhoodForm, AddPostForm, PasswordChangeForm, UpdateProfileForm, UpdateUserForm
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
+from Core import settings
+import threading
 
-# Create your views here.
+class EmailThread(threading.Thread):
+
+    def __init__(self, email):
+        self.email = email
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.email.send()
+
+def send_activation_email(user, request):
+    current_site = get_current_site(request)
+    email_subject = 'Activate Your Neighbourhood Account'
+    email_body = render_to_string('Account Activation Email.html', {
+        'user': user,
+        'domain': current_site,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user)
+    })
+
+    email = EmailMessage(subject=email_subject, body=email_body,
+    from_email=settings.EMAIL_FROM_USER, to=[user.email])
+
+    if not settings.TESTING:
+        EmailThread(email).start()
+
