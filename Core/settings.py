cf0121a57a1df057c0df6a42fee26224ad94a2ac
ian-otieno <ian.otieno@student.moringaschool.com>
2022-04-16 10:51:22 +0300

    add counties and choices to models.py

--- Core/settings.py
+++ Core/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Neighbourhood',
+    'cloudinary',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
