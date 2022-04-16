df5f514dd5f70ce03a5d4fe72d5b50b0d705fbba
ian-otieno <ian.otieno@student.moringaschool.com>
2022-04-16 11:48:10 +0300

    add class add post  to forms.py

--- Neighbourhood/forms.py
+++ Neighbourhood/forms.py
@@ -128,3 +128,12 @@ class AddNeighbourhoodForm(forms.ModelForm):
     class Meta:
         model = NeighbourHood
         fields = ('title','description','location','county','neighbourhood_logo','health_department','police_department')
+class AddPostForm(forms.Form):
+    title = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control mb-4', 'placeholder':'Post Title'}))
+    description = forms.CharField(required=True, widget=forms.Textarea(attrs={'class': 'form-control mb-4', 'rows': 5, 'placeholder':'Description'}))
+    category = forms.ChoiceField(choices=CATEGORIES, required=True, widget=forms.Select(attrs={'class': 'form-control mb-4', 'placeholder':'Select Category'}))
+    neighbourhood = forms.ChoiceField(label=u'Select Your Neighbourhood', required=True, widget=forms.Select(attrs={'class': 'form-control mb-4'}))
+
+    def __init__(self, *args, **kwargs):
+        super(AddPostForm, self).__init__(*args, **kwargs)
+        self.fields['neighbourhood'].choices = [(e.id, e.title) for e in NeighbourHood.objects.all()]
